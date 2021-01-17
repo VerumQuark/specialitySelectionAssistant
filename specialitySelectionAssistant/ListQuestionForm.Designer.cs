92330bc6870c8519901eee3e698c9f6c46ff8f9d
Константин Чичук <chichukko@gmail.com>
2021-01-17 19:35:53 +0200

    Add list skip func.

--- specialitySelectionAssistant/ListQuestionForm.Designer.cs
+++ specialitySelectionAssistant/ListQuestionForm.Designer.cs
@@ -212,6 +212,8 @@ namespace specialitySelectionAssistant
             this.button1.TabIndex = 23;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
             // ListQuestionForm
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
